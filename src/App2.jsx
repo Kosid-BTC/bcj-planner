f9c98a5a5122416f395d950f8b4b6ada77e75407
ฺB.CJ <support@b-tctraining.com>
2026-04-03 07:25:46 +0700

    Remove Vite configuration from App2.jsx

--- src/App2.jsx
+++ src/App2.jsx
@@ -1,14 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    // กำหนดให้สร้าง output ไว้ในโฟลเดอร์ dist (Vercel จะหาโฟลเดอร์นี้เพื่อแสดงผล)
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  }
-})
