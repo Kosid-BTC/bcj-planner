cd3ba4878d75893c347fe24159298ecfa7f0f70d
ฺB.CJ <support@b-tctraining.com>
2026-04-03 07:49:59 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    // กำหนดให้สร้าง output ไว้ในโฟลเดอร์ dist (Vercel จะหาโฟลเดอร์นี้เพื่อแสดงผล)
-    outDir: 'dist',
-  },
-  server: {
-    port: 3000,
-  }
-})
+{
+  "rewrites": [{ "source": "/(.*)", "destination": "/index.html" }]
+}
